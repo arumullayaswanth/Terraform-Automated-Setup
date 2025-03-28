b46f46cb36acaa64178c18843981a673d337af5e
Arumulla Yaswanth Reddy <164642237+arumullayaswanth@users.noreply.github.com>
2025-03-28 15:00:52 +0530

    Update ec2_instance with variables.tf

--- ec2_instance with variables.tf
+++ ec2_instance with variables.tf
@@ -3,8 +3,8 @@ provider "aws" {
 }
 
 resource "aws_instance" "one" {
-  count         = var.instance_count  # Number of instances to create
-  ami           = "ami-0b41f7055516b991a"  # Amazon Linux 2 AMI (ap-south-1)
+  count = var.instance_count  # Number of instances to create
+  ami = "ami-0b41f7055516b991a"  # Amazon Linux 2 AMI (ap-south-1)
   instance_type = var.instance_type  # Instance type (customizable)
 
   tags = {
@@ -14,14 +14,14 @@ resource "aws_instance" "one" {
 
 variable "instance_type" {
   description = "Instance type to be used (e.g., t2.micro, t3.medium)"
-  type        = string
-  default     = "t2.micro"  # Default value
+  type = string
+  default = "t2.micro"  # Default value
 }
 
 variable "instance_count" {
   description = "Number of EC2 instances to launch"
-  type        = number
-  default     = 5  # Default value
+  type = number
+  default = 5  # Default value
 }
 
 
